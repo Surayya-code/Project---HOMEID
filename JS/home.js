4f1b04eaea808fbb283b6f87fa8d74f38c96adaa
surayya-code <surayyaah@code.edu.az>
2021-04-30 10:54:25 +0400

    sections created

--- JS/home.js
+++ JS/home.js
@@ -1,48 +1,37 @@
 $(document).ready(function () {
-  // $('.owl-carousel').owlCarousel({
-  //     // loop:true,
-  //     // margin:10,
-  //     // nav:true,
-  //     // singleItem:true,
-  //     responsive:{
-  //         0:{
-  //             items:1
-  //         },
-  //         600:{
-  //             items:3
-  //         },
-  //         1000:{
-  //             items:5
-  //         }
-
-  //     },
-  //     navigation : true, // Show next and prev buttons
-  //     slideSpeed : 300,
-  //     paginationSpeed : 400,
-  //     singleItem: true
-  //})
-  $("#owl-demo").owlCarousel({
+  $(".mySlider").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
     responsive: {
       0: {
         items: 1,
+        dots: true,
       },
       600: {
-        items: 1,
+        items: 3,
       },
       1000: {
         items: 1,
       },
     },
-    navigation: true, // Show next and prev buttons
-    slideSpeed: 300,
-    paginationSpeed: 400,
-    singleItem: true,
-
-    // "singleItem:true" is a shortcut for:
-    items: 1,
-    // itemsDesktop: false,
-    // itemsDesktopSmall: false,
-    // itemsTablet: false,
-    // itemsMobile: false,
+  });
+  
+  $(".carousel-item").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
   });
 });
